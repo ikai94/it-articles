faefb18aef176d2dfcebe9ea1cb5e852f37052af
Anton Kotylev <theantony1994@yandex.ru>
2023-07-27 16:45:59 +0300

    add babel, add extract plagins

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,21 +2,48 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
+
+
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        "plugins": [
+          [
+            "i18next-extract",
+             {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true 
+             }
+          ],
+        ]
+      }
+    }
+  }
+
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
-        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
       {
-        loader: 'css-loader',
+        loader: "css-loader",
         options: {
           modules: {
-            auto: (resPath: string) => Boolean(resPath.includes('.module')),
-            localIdentName: isDev 
-              ? '[path][name]__[local]--[hash:base64:5]' 
-              : '[hash:base64:8]'
+            auto: (resPath: string) => Boolean(resPath.includes(".module")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
           },
-        }
+        },
       },
       "sass-loader",
     ],
@@ -29,8 +56,20 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   };
 
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
+
   return [
-    typescriptLoader, 
-    cssLoaders
-  ]
+    fileLoader,
+    svgLoader,
+    babelLoader,
+    typescriptLoader,
+    cssLoaders,
+    ];
 }
---
+++ src/shared/lib/classNames/classNames.ts
@@ -0,0 +1,14 @@
+type Mods = Record<string, boolean | string>
+
+export function classNames(cls:string, mods: Mods = {}, additional: string[] = []):string {
+   return [
+    cls,
+    ...additional.filter(Boolean),
+    ...Object.entries(mods)
+    .filter(([classNames, value]) => Boolean(value))
+    .map(([classNames]) => classNames)
+   ]
+   .join(' ');
+}
+
+classNames('remove-btn', {hovered: true, selectable: true, red: false}, ['pdg'])
