0c7048fd385a697f588553ca5e0a7119211a4f3a
Anton Kotylev <theantony1994@yandex.ru>
2023-08-06 18:10:50 +0300

    add react testing library, add tests for btn, sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -18,6 +18,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
     return (
         <div
+            data-testid="sidebar"
             className={classNames(
                 cls.Sidebar,
                 { [cls.collapsed]: collapsed },
@@ -25,7 +26,12 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 [className],
             )}
         >
-            <Button onClick={onToggle}>toggle</Button>
+            <Button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+            >
+                toggle
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
