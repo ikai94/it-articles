6ce4fb4e15b8b40cb9b4dbd00956a25299e76f9f
Anton Kotylev <theantony1994@yandex.ru>
2024-01-07 20:11:58 +0300

    mini fix

--- src/shared/ui/redesigned/Skeleton/Skeleton.tsx
+++ src/shared/ui/redesigned/Skeleton/Skeleton.tsx
@@ -8,10 +8,7 @@ interface SkeletonProps {
     width?: string | number;
     border?: string;
 }
-/**
- * Устарел, используем новые компоненты из папки redesigned
- * @deprecated
- */
+
 export const Skeleton = memo((props: SkeletonProps) => {
     const { className, height, border, width } = props;
 
